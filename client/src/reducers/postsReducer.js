4bd6af75447d2a897c53fdbc3b1053daf7bf111e
Jamie S <jamieshi2u@gmail.com>
2018-09-06 03:06:36 -0700

    Add tests for posts reducer

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -7,15 +7,11 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.GET_POSTS:
-      console.log('GET POSTS ACTION', action);
-      console.log('GET POSTS STATE', state);
       return {
         ...state,
         posts: action.payload
       };
     case types.CREATE_POST: {
-      console.log('CREATE POST ACTION', action);
-      console.log('CREATE POST STATE', state);
       return {
         ...state,
         posts: [
@@ -29,12 +25,10 @@ export default (state = initialState, action) => {
       };
     }
     case types.UPDATE_POST: {
-      console.log('CALLED UPDATE POST', action);
       return {
         ...state,
         posts: state.posts.map((post) => {
           if (post._id === action.id) {
-            console.log('FOUND THE ID', post, 'changing to', action.text);
             return {
               ...post,
               text: action.text,
@@ -46,8 +40,6 @@ export default (state = initialState, action) => {
       };
     }
     case types.DELETE_POST: {
-      console.log('DELETE POST STATE', state);
-      console.log('DELETE POST ACTION', action);
       return {
         ...state,
         posts: state.posts.filter(({ _id }) => _id !== action.id)
---
+++ client/src/tests/reducers/postsReducer.test.js
@@ -0,0 +1,103 @@
+import * as types from '../../actions/actionTypes';
+import postsReducer from '../../reducers/postsReducer';
+
+const initialState = {
+  posts: [
+    {
+      author: 'Jim',
+      text: 'Hello world!',
+      _id: '123'
+    }
+  ]
+};
+
+describe('posts reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      postsReducer(initialState, {
+        ...initialState,
+        posts: [
+          {
+            author: 'Jim',
+            text: 'Hello world!',
+            _id: '123'
+          }
+        ]
+      })
+    ).toEqual(initialState);
+  });
+
+  it('should handle GET_POSTS', () => {
+    const action = {
+      type: types.GET_POSTS,
+      payload: initialState.posts
+    };
+    expect(postsReducer(initialState, action)).toEqual({
+      ...initialState,
+      posts: [
+        {
+          author: 'Jim',
+          text: 'Hello world!',
+          _id: '123'
+        }
+      ]
+    });
+  });
+
+  it('should handle CREATE_POST', () => {
+    const action = {
+      type: types.CREATE_POST,
+      payload: {
+        author: 'Wendy',
+        text: 'How are you?',
+        _id: '1000'
+      }
+    };
+    expect(postsReducer(initialState, action)).toEqual({
+      ...initialState,
+      posts: [
+        {
+          author: 'Jim',
+          text: 'Hello world!',
+          _id: '123'
+        },
+        {
+          author: 'Wendy',
+          text: 'How are you?',
+          _id: '1000'
+        }
+      ]
+    });
+  });
+
+  it('should handle UPDATE_POST', () => {
+    const action = {
+      type: types.UPDATE_POST,
+      author: 'Jim',
+      text: 'Updated text',
+      id: '123'
+    };
+
+    expect(postsReducer(initialState, action)).toEqual({
+      ...initialState,
+      posts: [
+        {
+          author: 'Jim',
+          text: 'Updated text',
+          _id: '123'
+        }
+      ]
+    });
+  });
+
+  it('should handle DELETE_POST', () => {
+    const action = {
+      type: types.DELETE_POST,
+      id: '123'
+    };
+    expect(postsReducer(initialState, action)).toEqual({
+      ...initialState,
+      posts: []
+    });
+  });
+});
